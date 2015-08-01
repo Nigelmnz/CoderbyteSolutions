ff71118ab5fc01b84844fd97dc98be73c35f9691
nigel <nigelmnz@gmail.com>
2015-08-01 00:02:58 -0400

    8

--- 08-CheckNums/8.js
+++ 08-CheckNums/8.js
@@ -1,9 +1,5 @@
 function CheckNums(num1,num2) { 
-  if(num1 === num2){
-    return -1;
-  }
-  
-  return num1 < num2;
+  return (num1 === num2) ? -1 : num1 < num2;
 }
 
 console.log(CheckNums(4,7) === true)
