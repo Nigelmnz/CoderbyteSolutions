28c9570f98170088bd22d34fb2c40d2ea5f5404f
nigel <nigelmnz@gmail.com>
2015-08-03 22:24:57 -0400

    16

--- 16-ArithGeo/16.js
+++ 16-ArithGeo/16.js
@@ -10,5 +10,3 @@ console.log(ArithGeo([2,4,6,8])   === "Arithmetic")
 console.log(ArithGeo([2,4,6,10])  === -1)
 console.log(ArithGeo([3,9,27,81]) === "Geometric")
 console.log(ArithGeo([1,4,16,20])  === -1)
-
-[x,x+c,(x+c*2) = (x+c)*d,(x+c*2)*d*d != (x+c*3c)]
